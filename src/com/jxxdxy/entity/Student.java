64e487aa82a2730364d2ce454c3c0b8b94d90eee
xf <xf@jxxdxy.com>
2020-09-26 21:10:21 +0800

    update

--- src/com/jxxdxy/entity/Student.java
+++ src/com/jxxdxy/entity/Student.java
@@ -5,36 +5,5 @@ public class Student {
     private String name;
     private Integer age;
 
-    public Student() {
-    }
 
-    public Student(int id, String name, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
